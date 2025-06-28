8c19f9e0d25cc7f0d52e258f4417f4fd8dbdae64
TirthParkhiya <parkhiyatirth29@gmail.com>
2025-06-28 17:13:38 +0530

    Flask App

--- application.py
+++ application.py
@@ -33,4 +33,4 @@ def pridict_datapoint():
         return render_template('home.html',result=result[0])
 
 if __name__=='__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",debug=True)
